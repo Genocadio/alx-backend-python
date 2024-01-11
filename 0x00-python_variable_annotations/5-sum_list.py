7e80f485e48ad43b23823115470ab6d791f03d65
geno <yvesgeno@outlook.com>
2024-01-11 21:49:46 +0200

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""_function to sum a list of numbers in list"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """_function to sum a list of numbers"""
+    return sum(input_list)
