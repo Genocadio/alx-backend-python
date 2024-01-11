d9edbcdb5b702998bfd2195e71e64ef399d7c07f
geno <yvesgeno@outlook.com>
2024-01-11 22:30:08 +0200

    task 9 correction

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ function that returns list of tuples"""
 
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """function that returns list of tuples"""
     return [(i, len(i)) for i in lst]
