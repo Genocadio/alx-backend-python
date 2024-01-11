e015719e8374c27f9a8980e8a75d0b606cba57f4
geno <yvesgeno@outlook.com>
2024-01-11 22:42:17 +0200

    task 100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that returns the first element of iterable if it exists"""
+
+from typing import Iterable, Optional, Any
+
+
+def safe_first_element(lst: Iterable[Any]) -> Optional[Any]:
+    """Returns the first element of the iterable if it exists"""
+    if lst:
+        return next(iter(lst))
+    else:
+        return None
