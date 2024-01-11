83a2709959380ec4e48061a68784f944c25533c6
geno <yvesgeno@outlook.com>
2024-01-11 22:12:48 +0200

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function to return a multiplier"""
+
+
+def make_multiplier(multiplier: float) -> float:
+    """function to return a multiplier"""
+    return multiplier * 2
