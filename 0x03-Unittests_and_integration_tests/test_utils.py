3676ec92c3604da57fd4955c9738816ee1b32421
geno <yvesgeno@outlook.com>
2024-02-05 22:30:22 +0200

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -61,3 +61,28 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**atrbs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self) -> None:
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            test = TestClass()
+            self.assertEqual(test.a_property, 42)
+            self.assertEqual(test.a_property, 42)
+            mock.assert_called_once()
+            mock.reset_mock()
+            test.a_property = 98
+            self.assertEqual(test.a_property, 98)
+            mock.assert_not_called()
+            mock.reset_mock()
+            test.a_method = 98
+            self.assertEqual(test.a_method, 98)
+            mock.assert_not_called()
