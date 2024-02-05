6ae17392173e1f60c7a59defb9fd74a7e460ba75
geno <yvesgeno@outlook.com>
2024-02-05 22:18:43 +0200

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,3 +31,16 @@ class TestAccessNestedMap(unittest.TestCase):
         expected: Union[Dict, int]
     ) -> None:
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+        self,
+        nested_map: Dict,
+        path: Tuple[str],
+        exception: Exception,
+    ) -> None:
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
